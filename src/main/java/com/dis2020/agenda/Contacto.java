058816c02e22700293b25ed78686dfedff8d412e
yaguetskr <cabelloyago@gmail.com>
2020-04-26 19:29:38 +0200

    Añadida clase contacto

---
+++ src/main/java/com/dis2020/agenda/Contacto.java
@@ -0,0 +1,49 @@
+package com.dis2020.agenda;
+
+public class Contacto {
+	String nombre;
+	String apellidos;
+	String empresa;
+	String telefono;
+	String email;
+	String direccion;
+	
+	public Contacto(String nom,String ape,String emp,String tlf,String mail,String dir) {
+		
+		nombre=nom;
+		apellidos=ape;
+		empresa=emp;
+		telefono=tlf;
+		email=mail;
+		direccion=dir;
+		
+		
+	}
+	
+	public void setnombre(String ent) {
+		nombre=ent;
+	}
+	
+	public void setapellidos(String ent) {
+		apellidos=ent;
+	}
+	
+	public void setempresa(String ent) {
+		empresa=ent;
+	}
+	
+	public void settelefono(String ent) {
+		telefono=ent;
+	}
+	
+	public void setemail(String ent) {
+		email=ent;
+	}
+	
+	public void setdireccion(String ent) {
+		direccion=ent;
+	}
+	
+
+
+}
