f50410b7e83675fd28c8162ad666a080434c7044
yaguetskr <cabelloyago@gmail.com>
2020-04-27 19:44:18 +0200

    Tabla

--- src/main/java/com/dis2020/agenda/MyUI.java
+++ src/main/java/com/dis2020/agenda/MyUI.java
@@ -1,7 +1,11 @@
 package com.dis2020.agenda;
 
-import javax.servlet.annotation.WebServlet;
+import java.util.Arrays;
 
+import javax.servlet.annotation.WebServlet;
+import com.vaadin.ui.Grid;
+import java.util.ArrayList;
+import java.util.List;
 import com.vaadin.annotations.Theme;
 import com.vaadin.annotations.VaadinServletConfiguration;
 import com.vaadin.server.VaadinRequest;
@@ -11,6 +15,7 @@ import com.vaadin.ui.Label;
 import com.vaadin.ui.TextField;
 import com.vaadin.ui.UI;
 import com.vaadin.ui.VerticalLayout;
+import java.util.Arrays;
 
 /**
  * This UI is the application entry point. A UI may either represent a browser window 
@@ -25,6 +30,7 @@ public class MyUI extends UI {
     @Override
     protected void init(VaadinRequest vaadinRequest) {
         final VerticalLayout layout = new VerticalLayout();
+        layout.setSizeFull();
         
         final TextField name = new TextField();
         name.setCaption("Type your name here:");
@@ -35,7 +41,31 @@ public class MyUI extends UI {
                     + ", it works!"));
         });
         
-        layout.addComponents(name, button);
+        List<Contacto> contacts =new ArrayList<Contacto>();
+        
+        Contacto temp = new Contacto("Aurelio","Sanchez de la serna","microsoft","999999","pipo@gmail.com","Calle falsa numero 8");
+       	contacts.add(temp);
+        	
+        		
+        		
+        
+        
+        
+        Grid<Contacto> tabla = new Grid<>("Contactos");
+        tabla.setSizeFull();
+        tabla.setItems(contacts);
+        tabla.addColumn(Contacto::getnombre).setCaption("Nombre");
+        tabla.addColumn(Contacto::getapellidos).setCaption("Apellidos");
+        tabla.addColumn(Contacto::getempresa).setCaption("Empresa");
+        tabla.addColumn(Contacto::gettelefono).setCaption("Telefono");
+        tabla.addColumn(Contacto::getemail).setCaption("Email");
+        tabla.addColumn(Contacto::getdireccion).setCaption("Direccion");
+        
+        
+        
+        
+        
+        layout.addComponents(tabla);
         
         setContent(layout);
     }
